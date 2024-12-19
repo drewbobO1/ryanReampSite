b9cdb95dc0dd1bcddf3201e293fb9dc7aef952a1
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-12-19 16:37:48 -0600

    feat: Flesh out contact btn prompt popover

--- src/main.js
+++ src/main.js
@@ -1,24 +1,76 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const contactBtn = document.getElementById('contactBtn');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+contactBtn.addEventListener('click', () => {
+  handleContactBtn();
+});
 
-setupCounter(document.querySelector('#counter'))
+
+
+function handleContactBtn() {
+  console.log("contactBtn pressed!");
+
+  const backdrop =  document.createElement('div');
+  backdrop.classList.add('backdrop');
+  backdrop.id = "backdrop";
+
+  backdrop.addEventListener('click', () => {
+    backdrop.classList.remove("show");
+    promptWindowContainer.classList.remove("show");
+  })
+
+  const promptWindowContainer = document.createElement('div');
+  promptWindowContainer.classList.add('prompt-window-container');
+  promptWindowContainer.id = "promptWindowContainer";
+
+  
+
+  const promptWindow = document.createElement('div');
+  promptWindow.classList.add('bg-white', 'no-hover');
+  promptWindowContainer.appendChild(promptWindow);
+  
+  const titleBar = document.createElement('div');
+  titleBar.classList.add('title-bar');
+  promptWindow.appendChild(titleBar);
+
+  const windowBody = document.createElement('div');
+  windowBody.classList.add('window-body');
+  windowBody.style.display = "flex";
+  windowBody.style.flexDirection = "column";
+  promptWindow.appendChild(windowBody);
+
+  const promptText = document.createElement('p');
+  promptText.innerText = "Before proceeding, please read our guide on how to send us files properly";
+  windowBody.appendChild(promptText);
+
+  const buttonSection = document.createElement('section');
+  buttonSection.classList.add('field-row');
+  buttonSection.style.justifyContent = "flex-end";
+  
+  const viewPDFBtn = document.createElement('button');
+  viewPDFBtn.textContent = "Delete!";
+  viewPDFBtn.addEventListener('click', () => {
+
+    
+
+    backdrop.classList.remove("show");
+    promptWindowContainer.classList.remove("show");
+  })
+
+
+  const continueToEmailBtn = document.createElement('button');
+  continueToEmailBtn.textContent = "No, keep it";
+  continueToEmailBtn.addEventListener('click', () => {
+    backdrop.classList.remove("show");
+    promptWindowContainer.classList.remove("show");
+  })
+  
+  buttonSection.appendChild(viewPDFBtn);
+  buttonSection.appendChild(continueToEmailBtn);
+  windowBody.appendChild(buttonSection);
+
+  backdrop.classList.add("show");
+  promptWindowContainer.classList.add("show");
+
+  document.body.appendChild(backdrop);
+  document.body.appendChild(promptWindowContainer);
+}
